501b20962d24a532e80b3fc22eeb16128aa9d1bc
Pavel Alay <pavel@alay.by>
2024-06-20 10:28:13 +0000

    chore: Update Dockerfile and nodeserver.js

--- exercisefiles/node/nodeserver.js
+++ exercisefiles/node/nodeserver.js
@@ -5,3 +5,54 @@
 // if the url has other methods, return "method not supported"
 // when server is listening, log "server is listening on port 3000"
 
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const { pathname, query } = parsedUrl;
+
+    if (pathname === '/get') {
+        console.log(query);
+        const key = query.key;
+        if (!key) {
+            res.end('key not passed');
+        } else {
+            res.end(`hello ${key}`);
+        }
+    } else if (pathname === '/daysBetweenDates') {
+        const { startDate, endDate } = query;
+        if (!startDate || !endDate) {
+            res.end('start date or end date not passed\n');
+        } else {
+            const start = new Date(startDate);
+            const end = new Date(endDate);
+            const timeDiff = Math.abs(end.getTime() - start.getTime());
+            const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+            res.end(`${daysDiff}`);
+
+        }
+    } else if (pathname === '/validatephonenumber') {
+        const phoneNumber  = query.phoneNumber;
+        if (!phoneNumber) {
+            res.end('phone number not passed');
+        } else {
+            // Perform phone number validation logic here
+            // You can use regular expressions or any other validation library
+            // For example, using a regular expression to validate a 10-digit US phone number
+            const phoneNumberRegex = /^\+34\d{9}$/;
+            if (phoneNumberRegex.test(phoneNumber)) {
+                res.end('valid');
+            } else {
+                console.log(phoneNumber);
+                res.end('invalid');
+            }
+        }
+    } else {
+        res.end('method not supported');
+    }
+});
+
+server.listen(3000, () => {
+    console.log('server is listening on port 3000');
+});
--- exercisefiles/node/test.js
+++ exercisefiles/node/test.js
@@ -1,9 +1,3 @@
-//write npm command line to install mocha
-//npm install --global mocha
-
-//command to run this test file
-//mocha test.js
-
 const assert = require('assert');
 const http = require('http');
 const server = require('./nodeserver');
@@ -11,30 +5,85 @@ const server = require('./nodeserver');
 describe('Node Server', () => {
     it('should return "key not passed" if key is not passed', (done) => {
         http
-        .get('http://localhost:3000/get' , (res) => {
+            .get('http://localhost:3000/get', (res) => {
+                let data = '';
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+                res.on('end', () => {
+                    assert.equal(data, 'key not passed');
+                    done();
+                });
+            });
+    });
+
+    it('should return hello plus the key', (done) => {
+        const keys = ['fizz', 'buzz', 'foo', 'bar'];
+        const expectedResponses = keys.map(key => `hello ${key}`);
+        let count = 0;
+
+        keys.forEach((key, index) => {
+            http.get(`http://localhost:3000/get?key=${key}`, (res) => {
+                let data = '';
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+                res.on('end', () => {
+                    assert.equal(data, expectedResponses[index]);
+                    count++;
+                    if (count === keys.length) {
+                        done();
+                    }
+                });
+            });
+        });
+    });
+
+    it('should return the number of days between two dates', (done) => {
+        const startDate = new Date('2022-01-01');
+        const endDate = new Date('2022-01-10');
+        const expectedDays = 9;
+        http.get(`http://localhost:3000/daysBetweenDates?startDate=${startDate}&endDate=${endDate}`, (res) => {
             let data = '';
             res.on('data', (chunk) => {
                 data += chunk;
             });
             res.on('end', () => {
-                assert.equal(data, 'key not passed');
+                assert.equal(data, expectedDays.toString());
                 done();
             });
         });
     });
 
-    //add test to check get when key is equal to world
+    it('should validate phone numbers', (done) => {
+        const keys = ['+48571123123', '+34571123123'];
+        const expectedResponses = ['invalid', 'valid'];
+        let count = 0;
 
-    //add test to check validatephoneNumber
+        keys.forEach((key, index) => {
+            http.get(`http://localhost:3000/validatephonenumber?phoneNumber=${encodeURIComponent(key)}`, (res) => {
+                let data = '';
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+                res.on('end', () => {
+                    assert.equal(data, expectedResponses[index], `Failed for ${key}`);
+                    count++;
+                    if (count === keys.length) {
+                        done();
+                    }
+                });
+            });
+        });
+    });
+
+    //add test to check valiatephoneNumber
 
     //write test to validate validateSpanishDNI
-   
+
 
     //write test for returnColorCode red should return code #FF0000
 
 
-   //write test for daysBetweenDates
-
-
-
+    //write test for daysBetweenDates
 });
